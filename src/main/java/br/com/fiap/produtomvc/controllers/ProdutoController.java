249890b7cb45b41d421cf759e10891590e9d3706
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 09:48:17 -0300

    feat: product list

--- src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
+++ src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
@@ -2,12 +2,15 @@ package br.com.fiap.produtomvc.controllers;
 
 import br.com.fiap.produtomvc.model.ProdutoModel;
 import br.com.fiap.produtomvc.repository.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/produtos")
@@ -26,11 +29,26 @@ public class ProdutoController {
     }
 
     @PostMapping("/salvar")
-    public String insertProduto(ProdutoModel produto) {
+    public String insertProduto(@Valid  ProdutoModel produto, BindingResult result,
+                                RedirectAttributes attributes) {
 
+        if(result.hasErrors()){
+
+            attributes.addFlashAttribute("mensagem", "nao foi possivel salvar");
+            return "redirect:/produtos/novo";
+
+        }
         repository.save(produto);
+        attributes.addFlashAttribute("mensagem", "Produto salvo com sucesso");
+
 
         return "redirect:/produtos/novo";
+    }
+
+    @GetMapping("/listar")
+    public String listarProdutos(Model model){
+        model.addAttribute("produtos", repository.findAll());
+        return "/produto/listar-produtos";
     }
 
 }
